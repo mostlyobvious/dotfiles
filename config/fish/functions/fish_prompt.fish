4be4168615364422482f0482fbf762e1b4a3e3d6
Paweł Pacana <pawel.pacana@gmail.com>
2026-07-01 09:29:02 +0200

    Show host in remote fish prompts
    * Add the host name to SSH fish prompts so VM and remote shells are visually distinct from local host terminals.
    
    * Keep local prompts unchanged by only adding the prefix when SSH session variables are present.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,3 +1,9 @@
 function fish_prompt --description Hydro
-    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
+    set --local remote
+
+    if set --query SSH_CONNECTION; or set --query SSH_TTY
+        set remote (prompt_hostname)' '
+    end
+
+    echo -e "$remote$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
 end
