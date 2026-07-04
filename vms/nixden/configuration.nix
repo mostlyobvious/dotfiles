4bec983ff3f1bbd4ff121718dd573d0025cfd275
Paweł Pacana <pawel.pacana@gmail.com>
2026-07-04 21:40:21 +0200

    Limit VM bootloader generations
    * Keep the Lima VM boot partition from filling with old systemd-boot
      entries and kernel/initrd images during repeated switches.

--- vms/nixden/configuration.nix
+++ vms/nixden/configuration.nix
@@ -15,6 +15,7 @@
   time.timeZone = "Europe/Warsaw";
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = false;
 
   fileSystems."/" = {
