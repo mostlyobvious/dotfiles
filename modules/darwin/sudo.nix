abc0c7fb0ac4ab65538e069b6a221dd50a9b04b5
Paweł Pacana <pawel.pacana@gmail.com>
2026-07-06 21:03:58 +0200

    Enable Touch ID for sudo
    * Manage /etc/pam.d/sudo_local so pam_tid survives macOS updates,
      rather than patching /etc/pam.d/sudo directly.

--- modules/darwin/sudo.nix
+++ modules/darwin/sudo.nix
@@ -4,4 +4,7 @@
   security.sudo.extraConfig = ''
     Defaults:${username} timestamp_timeout=30
   '';
+
+  # Touch ID for sudo, via /etc/pam.d/sudo_local so it survives OS updates.
+  security.pam.services.sudo_local.touchIdAuth = true;
 }
