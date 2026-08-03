a2d9e02937907020ad82d76a3d901695336ee244
Paweł Pacana <pawel.pacana@chattermill.io>
2026-08-03 17:23:44 +0200

    Install Cloud SQL proxy
    * Add the Cloud SQL proxy to the Kubernetes tooling module so it is
      available with the rest of the cluster access tools.

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -4,6 +4,7 @@
   home.packages = [
     pkgs.kubectl
     pkgs.argo-workflows
+    pkgs.google-cloud-sql-proxy
     (pkgs.google-cloud-sdk.withExtraComponents [
       pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
     ])
